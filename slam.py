c1b284af7ada5fa7952a24836a240d3d3dffe0a1
Murilo <eth0.db0@gmail.com>
2020-05-26 04:22:26 +0000

    Incomplete

--- slam.py
+++ slam.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python3
 import cv2
-import sdl2.ext
 import time
-
+from display import Display
 
 W = 1920//2
 H = 1080//2
 
-#pygame.init()
-#display = pygame.display.set_mode((W, H))
-#surface = pygame.Surface((W, H)).convert()
+disp = Display(W, H)
 
-sdl2.ext.init()
 
-window = sdl2.ext.Window("Hellow", size=(W, H))
-window.show()
+class FeatureExtractor(object):
+    GX = 16
+    GY = 12
+    def __init__(self):
+        self.orb = cv2.ORB_create()
+
+    def extract(self, img):
+        # run detect in grid
+        sy = img.shape[0]//self.GY
+        sx= img.shape[1]//self.GX
+        for ry in range(0, img.shape[0], sy):
+            for rx in range(0, img.shape[1], sx):
+                img_chunk = img[ry:ry+sy, rx:rx+sx]
+                kp = self.orb.detect(img_chunk, None)
+                for p in kp:
+                    print(p)
+
+fe = FeatureExtractor()
 
 def process_frame(img):
     img = cv2.resize(img, (W, H))
-    cv2.imshow('image: ', img)
+    fe.extract(img)
 
-    #surface.blit(img.swapaces(0,1))
-    print(img.shape)
-    print(img)
-
-    window.refresh()
-
-    """
-    pygame.pixelcopy.array_to_surface(surface, img.swapaxes(0,1))
-    pygame.draw.circle(display, (255,0,0), (10,10), 5, 1)
-    display.blit(surface, (0, 0))
-    pygame.display.update()
-    """
-    #print(img.shape)
+    #for p in kp:
+    #    u, v = map(lambda x: int(round(x)), p.pt)
+    #    cv2.circle(img, (u,v), color=(0, 255,0), radius=3)
+    disp.paint(img)
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture('test.mp4')
+
     while cap.isOpened():
         ret, frame = cap.read()
         if ret == True:
---
+++ slam2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import cv2
+import sdl2.ext
+import time
+
+
+W = 1920//2
+H = 1080//2
+
+#pygame.init()
+#display = pygame.display.set_mode((W, H))
+#surface = pygame.Surface((W, H)).convert()
+
+sdl2.ext.init()
+
+window = sdl2.ext.Window("Hellow", size=(W, H))
+window.show()
+
+def process_frame(img):
+    img = cv2.resize(img, (W, H))
+    events = sdl2.ext.get_events()
+    cv2.imshow('image', img)
+
+    #surface.blit(img.swapaces(0,1))
+    print(img.shape)
+    print(img)
+
+    window.refresh()
+
+    """
+    pygame.pixelcopy.array_to_surface(surface, img.swapaxes(0,1))
+    pygame.draw.circle(display, (255,0,0), (10,10), 5, 1)
+    display.blit(surface, (0, 0))
+    pygame.display.update()
+    """
+    #print(img.shape)
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture('test.mp4')
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret == True:
+            process_frame(frame)
+        else:
+            break
+
+
